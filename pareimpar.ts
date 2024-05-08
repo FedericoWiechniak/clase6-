555fcf2d218f13b75d9f6cf2b797b06fc30b4850
FedericoWiechniak <168236875+FedericoWiechniak@users.noreply.github.com>
2024-05-08 20:03:19 -0300

    Add files via upload

---
+++ pareimpar.ts
@@ -0,0 +1,23 @@
+import * as readlinesync from "readline-sync";
+
+function determinarparimpar(numer:number): string{
+    if(numero===0){
+        }return"el numero es 0";
+}else if (numero% 2 === 0){
+    return"el numero es par"
+}else{
+     return"el numero es impar";
+} 
+}
+
+const numeroingresado =readlinesync.question{"ingreseunnumero:"};
+const numero=parseInt(numero ingresado);
+
+const resultado=determinarparimpar(numero);}
+console.log(resultado);
+
+}
+}
+
+
+    )                                                                                                         
